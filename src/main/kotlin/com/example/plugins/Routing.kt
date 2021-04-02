9cc37336f4f1bcd28d8e0a471ff0b39dcbc65f70
jabez_magomere <jabez.magomere@twiga.com>
2021-04-02 15:27:08 +0300

    Initial repo setup

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,31 @@
+package com.example.plugins
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+
+fun Application.configureRouting() {
+
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
